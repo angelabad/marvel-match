b0a1a9465a5b9e12ff1e5f43adf002acebeffb26
Angel Abad <angelabad@gmail.com>
2018-11-22 13:23:39 +0100

    Configure vue basepath to gitlab folder if it exists

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+// vue.config.js file to be place in the root of your repository
+// make sure you update `yourProjectName` with the name of your GitLab project
+// FOR GITLAB PAGES
+
+module.exports = {
+    baseUrl: process.env.NODE_ENV === 'production'
+      ? '/marvel-tests/'
+      : '/'
+}
