686014de4f43259608c69e781d3f169e9500f7e1
Angel Abad <angelabad@gmail.com>
2018-12-20 08:49:05 +0100

    Add marvel style button with v aling

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -20,10 +20,10 @@
         <div class="column is-one-third">
           <Search v-on:sendHero="hero => this.hero1Id = hero" />
         </div>
-        <div class="column is-one-fifth">
+        <div class="column is-one-fifth has-vertically-aligned-content">
           <div v-if="hero1Id && hero2Id">
             <a
-              class="button is-info"
+              class="button aa--marvel-button"
               @click="showDetails(hero1Id, hero2Id)"
             >Match</a>
           </div>
@@ -35,6 +35,57 @@
     </section>
   </div>
 </template>
+
+<style scoped>
+.column.has-vertically-aligned-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.aa--marvel-button {
+  height: 44px;
+  background: #eb2328;
+  position: relative;
+  padding-left: 8px;
+  padding-top: 4px;
+  padding-bottom: 5px;
+  line-height: 24px;
+  display: inline-block;
+  color: white;
+  line-height: 16px;
+  font-family: DIN Next W01 Regular, Trebuchet MS, Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  font-size: 14px;
+  /*height: 16px;*/
+  line-height: 18px;
+  text-align: center;
+  text-transform: uppercase;
+  padding-top: 13px;
+  padding-right: 13px;
+  padding-bottom: 13px;
+  padding-left: 13px;
+}
+.aa--marvel-button:before {
+  content: "";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: -14px;
+  border-left: 15px solid #eb2328;
+  border-bottom: 15px solid transparent;
+  width: 0;
+}
+.aa--marvel-button:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: -14px;
+  border-right: 15px solid #eb2328;
+  border-top: 15px solid transparent;
+  width: 0;
+}
+</style>
 
 <script>
 import Search from './Search.vue'
