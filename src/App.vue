3d65abd5511dc5c867a4112fd32b619d6796dc78
Angel Abad <angelabad@gmail.com>
2018-12-20 09:11:08 +0100

    Add background image in app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.jpg">
+    <!--<img alt="Vue logo" src="./assets/logo.jpg">-->
     <Page />
   </div>
 </template>
@@ -18,11 +18,17 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /**margin-top: 60px;*/
+
+  background-image: url("./assets/background.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 50vh;
 }
 </style>
