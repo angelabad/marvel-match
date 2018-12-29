17a6282a3a8bbc3bb6b44a0b700070f6b96cee39
Angel Abad <angelabad@gmail.com>
2018-12-29 11:29:20 +0100

    Add leterers, cover artists, editors and urls

--- src/components/ComicDetails.vue
+++ src/components/ComicDetails.vue
@@ -17,20 +17,38 @@
           <div class="media-content">
             <h1 class="title is-spaced">{{comic.title}}</h1>
             <h2 class="subtitle">Published: {{ getReleaseDate }}</h2>
-            <p class="is-3">Issue Number: {{ comic.issueNumber }}</p>
-            <p class="is-3">Pages: {{ comic.pageCount }}</p>
+            <p class="is-3 has-text-white">Issue Number: {{ comic.issueNumber }}</p>
+            <p class="is-3 has-text-grey-light">Pages: {{ comic.pageCount }}</p>
+            <p class="is-3 has-text-white">Series: {{ comic.series.name }}</p>
             <p
               v-if="getWriterNames"
-              class="is-3"
+              class="is-3 has-text-grey-light"
             >Writers: {{ getWriterNames }}</p>
             <p
               v-if="getColoristNames"
-              class="is-3"
+              class="is-3 has-text-white"
             >Colorists: {{ getColoristNames }}</p>
             <p
+              v-if="getLettererNames"
+              class="is-3 has-text-grey-light"
+            >Letterers: {{ getLettererNames }}</p>
+            <p
+              v-if="getCoverArtistNames"
+              class="is-3 has-text-white"
+            >Cover Artist: {{ getCoverArtistNames }}</p>
+            <p
+              v-if="getEditorNames"
+              class="is-3 has-text-grey-light"
+            >Editors: {{ getEditorNames }}</p>
+            <p
               v-if="getCharacters"
-              class="is-3"
+              class="is-3 has-text-white"
             >Characters: {{ getCharacters }}</p>
+            <a
+              class="is-3"
+              :href="getMarvelUrl"
+              target="_blank"
+            >Go to Marvel</a>
           </div>
         </div>
 
@@ -38,34 +56,6 @@
           <p class="has-text-justified">
             <span v-html="comic.description"></span>
           </p>
-
-          <!--
-              <span class="tag is-link">blackbird was a sailor</span>
-              <span class="tag is-info">that's what you do</span>
-
-              <br><br>
-
-              <div class="field is-grouped is-grouped-multiline">
-                <div class="control">
-                  <div class="tags has-addons">
-                    <span class="tag is-dark">npm</span>
-                    <span class="tag is-info">0.5.0</span>
-                  </div>
-                </div>
-
-                <div class="field is-grouped is-grouped-multiline">
-                  <div class="control">
-                    <div class="tags has-addons">
-                      <span class="tag is-link">axiomaciae</span>
-                      <span class="tag is-info">24. 07. 2020</span>
-                    </div>
-                  </div>
-                  <br>
-                  <time datetime="2018-5-8">15:51 PM - 8 May 2018</time>
-
-                </div>
-              </div>
-              -->
         </div>
       </div>
     </div>
@@ -97,6 +87,15 @@ export default {
     getColoristNames: function () {
       return this.getCreatorNames('colorist')
     },
+    getLettererNames: function () {
+      return this.getCreatorNames('letterer')
+    },
+    getCoverArtistNames: function () {
+      return this.getCreatorNames('penciller (cover)')
+    },
+    getEditorNames: function () {
+      return this.getCreatorNames('editor')
+    },
     getReleaseDate: function () {
       if (!this.comic.dates) return ''
       const result = this.comic.dates.find(date => date.type === 'onsaleDate')
@@ -109,10 +108,13 @@ export default {
       this.comic.characters.items.forEach(character => names.push(character.name))
 
       return names.join(', ')
+    },
+    getMarvelUrl: function () {
+      // const resultado = inventario.find( fruta => fruta.nombre === 'cerezas' );
+      const url = this.comic.urls.find(url => url.type === 'detail')
+      if (url.length === 0) return ''
+      return url.url
     }
-  },
-  mounted: function () {
-    console.log(this.comic)
   }
 }
 </script>
