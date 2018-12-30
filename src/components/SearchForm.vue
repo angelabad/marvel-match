2708739417f964eb41a75cdefb7e65618a114676
Angel Abad <angelabad@gmail.com>
2018-12-30 17:56:38 +0100

    Remove html comments

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -39,16 +39,8 @@
         </div>
         <div class="card-content">
           <div class="media">
-            <!--
-                    <div class="media-left">
-                        <figure class="image is 48x48">
-                            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                        </figure>
-                    </div>
-            -->
             <div class="media-content">
               <p class="title is-4 aa--marvel-title-red">{{ hero.name }}</p>
-              <!-- <p class="subtitle is-6">{{ subName }}</p> -->
             </div>
           </div>
 
