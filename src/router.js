8d3c703ac42bed5ed9dd0e0bd58d4cfc7e716ecb
Angel Abad <angelabad@gmail.com>
2018-12-31 19:38:00 +0100

    Configure vue-router and paths

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Index from '@/components/Index'
+import SearchResults from '@/components/SearchResults'
+
+Vue.use(Router)
+
+const routes = [
+  { path: '/', component: Index },
+  { path: '/results', name: 'results', component: SearchResults }
+]
+
+const router = new Router({
+  mode: 'history',
+  routes
+})
+
+export default router
