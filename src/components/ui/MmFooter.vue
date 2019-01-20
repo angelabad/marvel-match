afba34c9bfed3ebf186b99c2c27f1ff3095b7868
Angel Abad <angelabad@gmail.com>
2019-01-20 13:00:36 +0100

    Create footer vue component

---
+++ src/components/ui/MmFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="aa-footer">
+    <div class="content has-text-centered is-size-7">
+      <p>
+        Coded with <span class="icon"><i class="fas fa-heart"></i></span> by <strong>Angel Abad</strong> | Data provided by Marvel. © 2019 Marvel.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.aa-footer {
+  position: absolute;
+  padding-bottom: 2px;
+  bottom: 0;
+  width: 100%;
+}
+</style>
