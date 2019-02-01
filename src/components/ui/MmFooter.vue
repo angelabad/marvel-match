5ab99d38e55f852128ddd65a576b00d194f79dff
Angel Abad <angelabad@gmail.com>
2019-02-01 16:55:19 +0100

    Update footer wiht bulma styles

--- src/components/ui/MmFooter.vue
+++ src/components/ui/MmFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="aa-footer">
+  <footer>
     <div class="content has-text-centered is-size-7">
       <p>
         Coded with <span class="icon"><i class="fas fa-heart"></i></span> by <strong>Angel Abad</strong> | Data provided by Marvel. © 2019 Marvel.
@@ -7,12 +7,3 @@
     </div>
   </footer>
 </template>
-
-<style scoped>
-.aa-footer {
-  position: absolute;
-  padding-bottom: 2px;
-  bottom: 0;
-  width: 100%;
-}
-</style>
