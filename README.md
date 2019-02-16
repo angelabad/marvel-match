85886ddd674dfa5010d92491a0403204902fdc9f
Angel Abad <angelabad@gmail.com>
2019-02-16 09:47:43 +0100

    Prepare links and brand to make the repo public

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# marvel
+# MarvelMatch
 
 ## Project setup
 
--- src/components/ui/MmFooter.vue
+++ src/components/ui/MmFooter.vue
@@ -2,7 +2,7 @@
   <footer>
     <div class="content has-text-centered is-size-7">
       <p>
-        Coded with <span class="icon"><i class="fas fa-heart"></i></span> by <a href="https://angelabad.me" target="_blank"><strong>Angel Abad</strong></a> | Data provided by Marvel. © 2019 Marvel.
+        <a href="https://gitlab.com/angelabad/marvel-match" target="_blank">Coded</a> with <span class="icon"><i class="fas fa-heart"></i></span> by <a href="https://angelabad.me" target="_blank"><strong>Angel Abad</strong></a> | Data provided by Marvel. © 2019 Marvel.
       </p>
     </div>
   </footer>
