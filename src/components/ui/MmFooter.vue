4a0a6776acb7f76032137e57063fae0221639dd4
Angel Abad <angelabad@gmail.com>
2019-02-27 08:35:59 +0100

    Update footer classes

--- src/components/ui/MmFooter.vue
+++ src/components/ui/MmFooter.vue
@@ -2,8 +2,17 @@
   <footer>
     <div class="content has-text-centered is-size-7">
       <p>
-        <a href="https://gitlab.com/angelabad/marvel-match" target="_blank">Coded</a> with <span class="icon"><i class="fas fa-heart"></i></span> by <a href="https://angelabad.me" target="_blank"><strong>Angel Abad</strong></a> | Data provided by Marvel. © 2019 Marvel.
+        <a href="https://gitlab.com/angelabad/marvel-match" target="_blank"><strong>Coded</strong></a> with <span class="icon"><i class="fas fa-heart"></i></span> by <a href="https://angelabad.me" target="_blank"><strong>Angel Abad</strong></a> | Data provided by Marvel. © 2019 Marvel.
       </p>
     </div>
   </footer>
 </template>
+
+<style scoped>
+p {
+  color: white !important;
+}
+a:visited, strong {
+  color: white !important;
+}
+</style>
