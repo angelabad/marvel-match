0e1c722c632976f5c5f71e1786dd4a3320b47732
Angel Abad <angelabad@gmail.com>
2019-02-28 10:35:23 +0100

    Use vue-mq to show different layouts in mobile or desktop screens

--- src/components/ComicDetails.vue
+++ src/components/ComicDetails.vue
@@ -1,94 +1,177 @@
 <template>
   <div id="ComicDetails">
 
-    <div class="card is-horizontal">
-      <div class="card-image">
+    <mq-layout mq="sm">
+      <section class="section">
+        <div class="columns is-mobile is-multiline">
+          <div class="column is-half">
+            <figure class="image has-background-black-ter">
+              <img
+                v-bind:src="comic.thumbnail.path + '.' + comic.thumbnail.extension | convertToHttps"
+                :alt="comic.title"
+              >
+            </figure>
+          </div>
+          <div class="column is-half">
+            <p class="subtitle is-4 has-text-weight-bold">{{comic.title}}</p>
+            <p><span class="has-text-weight-bold">Released:</span><br />{{ getReleaseDate }}</p>
+          </div>
 
-        <figure class="image has-background-black-ter">
-          <img
-            v-bind:src="comic.thumbnail.path + '.' + comic.thumbnail.extension | convertToHttps"
-            :alt="comic.title"
-            style="width: 100%; height: 600px; object-fit: contain;"
+          <div class="column is-full is-paddingless has-text-centered">
+            <p><a
+                class="button is-small is-black"
+                :href="getMarvelUrl"
+                target="_blank"
+              >Show in Marvel</a>
+            </p>
+          </div>
+
+          <div
+            v-if="comic.description"
+            class="column is-full has-text-justified"
           >
-        </figure>
-      </div>
-      <div class="card-content has-background-black-ter">
-        <div class="media">
-          <div class="media-content has-text-white">
-            <h1 class="title is-spaced">{{comic.title}}</h1>
+            <p class="has-text-weight-bold">Summary</p>
+            <p><span v-html="comic.description"></span></p>
+          </div>
 
-            <div class="columns">
-              <div
-                v-if="getReleaseDate"
-                class="column is-half"
-              >
-                <p><span class="has-text-weight-bold">Published:</span><br />{{ getReleaseDate }}</p>
+          <div v-if="comic.issueNumber" class="column is-half">
+            <p>
+            <span class="has-text-weight-bold">Issue Number:</span> {{ comic.issueNumber }}
+            </p>
+          </div>
+          <div v-if="comic.pageCount" class="column is-half">
+            <p><span class="has-text-weight-bold">Pages:</span> {{ comic.pageCount }}</p>
+          </div>
+
+
+          <div
+            v-if="getWriterNames"
+            class="column is-half"
+          >
+            <p><span class="has-text-weight-bold">Writers:</span><br />{{ getWriterNames }}</p>
+          </div>
+          <div
+            v-if="getPencillerNames"
+            class="column is-half"
+          >
+            <p><span class="has-text-weight-bold">Pencillers:</span><br />{{ getPencillerNames }}</p>
+          </div>
+
+          <div
+            v-if="getCoverArtistNames"
+            class="column is-half"
+          >
+            <p><span class="has-text-weight-bold">Cover Artist:</span><br />{{ getCoverArtistNames }}</p>
+          </div>
+          <div
+            v-if="getColoristNames"
+            class="column is-half"
+          >
+            <p><span class="has-text-weight-bold">Colorists:</span><br />{{ getColoristNames }}</p>
+          </div>
+
+          <div
+            v-if="getCharacters"
+            class="column is-full"
+          >
+            <p><span class="has-text-weight-bold">Characters:</span><br />{{ getCharacters }}</p>
+          </div>
+
+        </div>
+      </section>
+    </mq-layout>
+
+    <mq-layout mq="md+">
+
+      <div class="card is-horizontal">
+        <div class="card-image">
+
+          <figure class="image has-background-black-ter">
+            <img
+              v-bind:src="comic.thumbnail.path + '.' + comic.thumbnail.extension | convertToHttps"
+              :alt="comic.title"
+              style="width: 100%; height: 600px; object-fit: contain;"
+            >
+          </figure>
+        </div>
+        <div class="card-content has-background-black-ter">
+          <div class="media">
+            <div class="media-content has-text-white">
+              <h1 class="title is-spaced">{{comic.title}}</h1>
+
+              <div class="columns">
+                <div
+                  v-if="getReleaseDate"
+                  class="column is-half"
+                >
+                  <p><span class="has-text-weight-bold">Published:</span><br />{{ getReleaseDate }}</p>
+                </div>
+                <div class="column is-half">
+                  <a
+                    class="button is-medium is-black"
+                    :href="getMarvelUrl"
+                    target="_blank"
+                  >Show in Marvel</a>
+                </div>
               </div>
-              <div class="column is-half">
-                <a
-                  class="button is-medium is-black"
-                  :href="getMarvelUrl"
-                  target="_blank"
-                >Show in Marvel</a>
+
+              <div class="columns is-multiline">
+
+                <div class="column is-half">
+                  <p><span class="has-text-weight-bold">Issue Number:</span> {{ comic.issueNumber }}</p>
+                </div>
+                <div class="column is-half">
+                  <p><span class="has-text-weight-bold">Pages:</span> {{ comic.pageCount }}</p>
+                </div>
+
+                <div
+                  v-if="getWriterNames"
+                  class="column is-half"
+                >
+                  <p><span class="has-text-weight-bold">Writers:</span><br />{{ getWriterNames }}</p>
+                </div>
+                <div
+                  v-if="getPencillerNames"
+                  class="column is-half"
+                >
+                  <p><span class="has-text-weight-bold">Pencillers:</span><br />{{ getPencillerNames }}</p>
+                </div>
+
+                <div
+                  v-if="getCoverArtistNames"
+                  class="column is-half"
+                >
+                  <p><span class="has-text-weight-bold">Cover Artist:</span><br />{{ getCoverArtistNames }}</p>
+                </div>
+                <div
+                  v-if="getColoristNames"
+                  class="column is-half"
+                >
+                  <p><span class="has-text-weight-bold">Colorists:</span><br />{{ getColoristNames }}</p>
+                </div>
+
+                <div
+                  v-if="getCharacters"
+                  class="column is-full"
+                >
+                  <p><span class="has-text-weight-bold">Characters:</span><br />{{ getCharacters }}</p>
+                </div>
+
+                <div class="column is-full has-text-justified">
+                  <span v-html="comic.description"></span>
+                </div>
+
               </div>
+
             </div>
-
-            <div class="columns is-multiline">
-
-              <div class="column is-half">
-                <p><span class="has-text-weight-bold">Issue Number:</span> {{ comic.issueNumber }}</p>
-              </div>
-              <div class="column is-half">
-                <p><span class="has-text-weight-bold">Pages:</span> {{ comic.pageCount }}</p>
-              </div>
-
-              <div
-                v-if="getWriterNames"
-                class="column is-half"
-              >
-                <p><span class="has-text-weight-bold">Writers:</span><br />{{ getWriterNames }}</p>
-              </div>
-              <div
-                v-if="getPencillerNames"
-                class="column is-half"
-              >
-                <p><span class="has-text-weight-bold">Pencillers:</span><br />{{ getPencillerNames }}</p>
-              </div>
-
-              <div
-                v-if="getCoverArtistNames"
-                class="column is-half"
-              >
-                <p><span class="has-text-weight-bold">Cover Artist:</span><br />{{ getCoverArtistNames }}</p>
-              </div>
-              <div
-                v-if="getColoristNames"
-                class="column is-half"
-              >
-                <p><span class="has-text-weight-bold">Colorists:</span><br />{{ getColoristNames }}</p>
-              </div>
-
-              <div
-                v-if="getCharacters"
-                class="column is-full"
-              >
-                <p><span class="has-text-weight-bold">Characters:</span><br />{{ getCharacters }}</p>
-              </div>
-
-              <div class="column is-full has-text-justified">
-                <span v-html="comic.description"></span>
-              </div>
-
-            </div>
-
           </div>
         </div>
+
       </div>
 
-    </div>
+    </mq-layout>
 
   </div>
-
 </template>
 
 <script>
