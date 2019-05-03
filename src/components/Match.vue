175f340c8f8392fee0ed41a6b6a9ebe31a4bb53c
Angel Abad <angelabad@gmail.com>
2019-05-03 19:15:57 +0200

    Use aria-* attributes for pagination

--- src/components/Match.vue
+++ src/components/Match.vue
@@ -102,6 +102,10 @@
                 :simple="isSimple"
                 :rounded="isRounded"
                 :per-page="perPage"
+                aria-next-label="Next page"
+                aria-previous-label="Previous page"
+                aria-page-label="Page"
+                aria-current-label="Current page"
               >
               </b-pagination>
             </div>
@@ -145,6 +149,10 @@
                 :simple="isSimple"
                 :rounded="isRounded"
                 :per-page="perPage"
+                aria-next-label="Next page"
+                aria-previous-label="Previous page"
+                aria-page-label="Page"
+                aria-current-label="Current page"
               >
               </b-pagination>
             </div>
