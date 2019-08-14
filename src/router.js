ccc5bbce4e6e7abdc09711e5815fb07ffa886a62
Angel Abad <angelabad@gmail.com>
2019-08-14 11:53:47 +0200

    Add optional parameter for pagination

--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ Vue.use(Router)
 
 const routes = [
   { path: '/', name: 'index', component: Index },
-  { path: '/match/:hero1/:hero2', name: 'match', component: Match },
+  { path: '/match/:hero1/:hero2/:page?', name: 'match', component: Match },
   { path: '*', component: NotFound }
 ]
 
