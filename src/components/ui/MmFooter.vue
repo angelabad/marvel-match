ac51d3e2c020fb9134c98af7941f348ece9339db
Angel Abad <angelabad@gmail.com>
2021-04-27 08:51:39 +0200

    Update copyright years

--- src/components/ui/MmFooter.vue
+++ src/components/ui/MmFooter.vue
@@ -2,7 +2,7 @@
   <footer>
     <div class="content has-text-centered is-size-7 has-text-white">
       <p>
-        <a href="https://gitlab.com/angelabad/marvel-match" target="_blank" rel="noopener"><strong>Coded</strong></a> with <font-awesome-icon :icon="['fas', 'heart']" size="sm"></font-awesome-icon> by <a href="https://angelabad.me" target="_blank" rel="noopener"><strong>Angel Abad</strong></a> | Data provided by Marvel. © 2019 Marvel.
+        <a href="https://gitlab.com/angelabad/marvel-match" target="_blank" rel="noopener"><strong>Coded</strong></a> with <font-awesome-icon :icon="['fas', 'heart']" size="sm"></font-awesome-icon> by <a href="https://angelabad.me" target="_blank" rel="noopener"><strong>Angel Abad</strong></a> | Data provided by Marvel. © 2019-2021 Marvel.
       </p>
     </div>
   </footer>
